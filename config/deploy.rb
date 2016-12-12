c4fd79481add3fe775ae5377eca803fd83b4f5ed
omerozturk0 <omer@localhost.localdomain>
2016-12-13 00:45:40 +0300

    deneme

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,46 +2,12 @@
 lock "3.7.0"
 
 set :user, "deploy"
-set :use_sudo, false
 
-set :scm, "git"
 set :application, "deploy_digitalocean"
 set :repo_url, "git@github.com:omerozturk0/deploy_digitalocean.git"
-set :branch, "master"
 
-set :ssh_options, { 
-  forward_agent: true, 
-  paranoid: true, 
-  keys: "~/.ssh/id_rsa" 
-}
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/deploy_digitalocean"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
